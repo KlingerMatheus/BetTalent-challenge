4ee5a825582a781f70d9bad92012ff23bd66a742
Klinger Matheus <kger.matheus@gmail.com>
2025-03-09 23:47:49 -0300

    chore: renamed file

--- src/type.d.ts
+++
@@ -1,7 +0,0 @@
-type User = {
-  photo?: string;
-  name: string;
-  role: string;
-  startDate: string;
-  phone: string;
-};
---
+++ src/types.d.ts
@@ -0,0 +1,8 @@
+type User = {
+  id: number;
+  image?: string;
+  name: string;
+  job: string;
+  admission_date: string;
+  phone: string;
+};
