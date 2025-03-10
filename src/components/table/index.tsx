2ee1e95fe60de990d878f1f9bb7e3725bd75197d
Klinger Matheus <kger.matheus@gmail.com>
2025-03-09 22:52:46 -0300

    chore: compose pattern to Table.

---
+++ src/components/table/index.tsx
@@ -0,0 +1,9 @@
+import TableContainer from "./TableContainer";
+import TableHead from "./TableHead";
+import TableItem from "./TableItem";
+
+export const Table = {
+  Head: TableHead,
+  Container: TableContainer,
+  Item: TableItem,
+};
