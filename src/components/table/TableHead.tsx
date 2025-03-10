572334b32355f4dbd02dc09c6f8d3bcd725e7a76
Klinger Matheus <kger.matheus@gmail.com>
2025-03-09 22:50:56 -0300

    feat: Creates `<TableHead />` component

---
+++ src/components/table/TableHead.tsx
@@ -0,0 +1,17 @@
+import styles from "./table.module.css";
+
+interface TableHeadProps {
+  columns: string[];
+}
+
+const TableHead = ({ columns }: TableHeadProps) => (
+  <thead className={styles.thead}>
+    <tr>
+      {columns.map((column) => (
+        <th>{column}</th>
+      ))}
+    </tr>
+  </thead>
+);
+
+export default TableHead;
